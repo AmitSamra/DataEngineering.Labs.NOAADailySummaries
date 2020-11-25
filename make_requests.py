4789041fe8cc0bf87383d04574a3abb06aae3c54
Amit Samra <Amit.Samra@outlook.com>
2020-11-25 17:34:21 -0500

    Added env var

--- make_requests.py
+++ make_requests.py
@@ -1,13 +1,20 @@
 import urllib.request
 import urllib.parse
 import json
+import os
+import json
+import pandas as pd
+from dotenv import load_dotenv
+dotenv_local_path = './.env'
+load_dotenv(dotenv_path=dotenv_local_path, verbose=True)
+
 
 file_counter = 0
 offset_counter = 1
 
 while file_counter < 2:
 
-	headers = {'token':'YgefhhEiwaNRdNofYQpeaquHVuPaoxyP'}
+	headers = {'token':os.environ.get('NOAA_Token')}
 
 	myurl = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=FIPS:10003&startdate=2018-12-01&enddate=2018-12-31&limit=1000&' + 'offset=' + str(offset_counter)
 	request = urllib.request.Request(myurl, headers = headers)
